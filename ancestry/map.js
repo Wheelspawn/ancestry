e788a895feb1a08244981eb0f95b3860d11e9681
Nathaniel Le Sage <nsage@localhost.localdomain>
2024-07-26 10:37:57 -0500

    first commit

--- ancestry/map.js
+++ ancestry/map.js
@@ -1,3 +1,5 @@
+const markers = [];
+var m = null;
 
 function initMap() {
     fetch('ancestry.json')
@@ -8,12 +10,15 @@ function initMap() {
             zoom: 4,
             center: location
         });
+        m = map;
         
         data.forEach(function(person) {
             var marker = new google.maps.Marker({
                 position: person.birth_loc,
                 map: map
             });
+            
+            markers.push([marker,person.birth_date,person.death_date]);
             
             var fullName = person.name.join(" ")
             var infoWindow = new google.maps.InfoWindow({
@@ -30,8 +35,30 @@ function initMap() {
             marker.addListener('click', function() {
                 infoWindow.open(map, marker);
             });
-    
+            
         });
-         
+        
     }).catch(error => console.error('Error loading JSON:', error));
+}
+
+function updateMap() {
+    var year_min = Date.parse(document.getElementById("year_min").value);
+    var year_max = Date.parse(document.getElementById("year_max").value);
+    
+    markers.forEach(function(marker) {
+    
+        var birth_date = Date.parse(marker[1].split(/-| /).pop());
+        var death_date = Date.parse(marker[2].split(/-| /).pop());
+        
+        if (birth_date > year_max || death_date < year_min) {
+            marker[0].setMap(null);
+        }
+        else {
+            console.log("Birth date: ", birth_date);
+            console.log("Death date: ", death_date);
+            console.log("Year min: ", year_min);
+            console.log("Year max: ", year_max);
+            marker[0].setMap(m);
+        }
+    });
 }
